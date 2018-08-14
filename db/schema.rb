0260a0b0bb0898770fc91c780c20ba2a6a7111a0
Kate <ms.katesy@gmail.com>
2018-08-14 10:31:48 +1000

    added join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_002356) do
+ActiveRecord::Schema.define(version: 2018_08_14_003103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2018_08_14_002356) do
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
+  create_table "games_users", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "game_id"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_games_users_on_game_id"
+    t.index ["user_id"], name: "index_games_users_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
@@ -33,4 +43,6 @@ ActiveRecord::Schema.define(version: 2018_08_14_002356) do
   end
 
   add_foreign_key "games", "users"
+  add_foreign_key "games_users", "games"
+  add_foreign_key "games_users", "users"
 end
